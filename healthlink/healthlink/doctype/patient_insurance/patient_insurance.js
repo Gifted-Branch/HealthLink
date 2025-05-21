98aeff7b00aa90abfdb6d0f6596ea23e86456487
Douglas-Nkubitu <nkubitudouglas@gmail.com>
2025-05-21 12:23:51 +0300

    feat: Implement dynamic query for scheme_name based on selected insurance_name in Patient Insurance

--- healthlink/healthlink/doctype/patient_insurance/patient_insurance.js
+++ healthlink/healthlink/doctype/patient_insurance/patient_insurance.js
@@ -1,8 +1,15 @@
 // Copyright (c) 2025, Douglas-Nkubitu and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Patient Insurance", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on('Patient Insurance', {
+    insurance_name: function (frm) {
+        frm.set_query('scheme_name', () => {
+            return {
+                filters: {
+                    insurance_company: frm.doc.insurance_name
+                  //  is_active: 1
+                }
+            };
+        });
+    }
+});
